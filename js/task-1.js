c273b534c5c2c3ec553a41f9990dd20e7934c342
yLevytsk <Lupin773311asd@gmail.com>
2025-01-18 16:38:17 +0000

    rgery

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,17 @@
 
-// Находим список категорий по ID
+
 const categoriesList = document.querySelector('#categories');
 
-// Считаем количество категорий (элементов li с классом item)
+
 const categoryItems = categoriesList.querySelectorAll('.item');
 console.log(`Number of categories: ${categoryItems.length}`);
 
-// Перебираем каждую категорию и выводим данные
+
 categoryItems.forEach((category) => {
-  // Находим заголовок h2 внутри категории
+  
   const categoryTitle = category.querySelector('h2').textContent;
 
-  // Считаем количество элементов в подсписке ul внутри категории
+  
   const elementsCount = category.querySelectorAll('ul li').length;
 
   console.log(`Category: ${categoryTitle}`);
