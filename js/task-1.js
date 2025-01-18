54b0104687d3c6cc152482fd9f4fa684e12715e1
yLevytsk <Lupin773311asd@gmail.com>
2025-01-18 15:04:47 +0000

    sfs

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+
+const categoriesList = document.querySelector('#categories');
+
+
+const categoryItems = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${categoryItems.length}`);
+
+
+categoryItems.forEach((category) => {
+ 
+  const categoryTitle = category.querySelector('h2').textContent;
+
+ 
+  const elementsCount = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elementsCount}`);
+});
